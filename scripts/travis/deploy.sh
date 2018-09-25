c0eeae608b2d6fb70b60f3bea5179a51e0fe79e9
Chris Harris <chris.harris@kitware.com>
2018-09-25 13:15:28 -0400

    WIP:Travis

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR=$(cd $(dirname $0) || exit 1; pwd)
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+${SCRIPT_DIR}/macpython-build-wheels.sh 3.5 
 echo 'deploy'
 echo $DEPLOY
