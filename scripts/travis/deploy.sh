da4089b4ebc7695cbd63b1210e0529ab5cb515b0
Chris Harris <chris.harris@kitware.com>
2018-09-25 16:37:31 -0400

    WIP: Don't install pip

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -3,8 +3,8 @@ set -e
 
 SCRIPT_DIR=$(cd $(dirname $0) || exit 1; pwd)
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python get-pip.py
+#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#sudo python get-pip.py
 ${SCRIPT_DIR}/macpython-build-wheels.sh 3.5 
 echo 'deploy'
 echo $DEPLOY
