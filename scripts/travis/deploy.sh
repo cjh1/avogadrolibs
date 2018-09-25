2cc06dbac69a2cd679d52ecb5f52e59578e44591
Chris Harris <chris.harris@kitware.com>
2018-09-25 16:55:34 -0400

    WIP: work around broken travis

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-set -e
-
-SCRIPT_DIR=$(cd $(dirname $0) || exit 1; pwd)
-
-#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-#sudo python get-pip.py
-${SCRIPT_DIR}/macpython-build-wheels.sh 3.5 
-echo 'deploy'
-echo $DEPLOY
+sudo pip install twine
+twine upload --repository-url  https://test.pypi.org/legacy/ dist/*
