937cd1e83c649801ee076b5c3652d180c216337d
Chris Harris <chris.harris@kitware.com>
2018-10-05 10:59:22 -0400

    WIP
    Signed-off-by: Chris Harris <chris.harris@kitware.com>

--- scripts/appveyor/upload-pypi.ps1
+++ scripts/appveyor/upload-pypi.ps1
@@ -1,4 +1,5 @@
 trap { Write-Error $_; Exit 1 }
 
-pip install twine
-twine upload dist/*
+pip install --disable-pip-version-check --user --upgrade pip
+pip install --user twine
+twine upload --repository-url  https://test.pypi.org/legacy/ dist/*
